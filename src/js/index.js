db08bad8c99badf85c8ad1a69bc5466788357d73
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-07-17 12:17:19 +0100

    Dynamically create project cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import "../css/index.css";
 import "../css/about.css";
 import "../css/projects.css";
+import { RenderProjects } from "./projects.js";
+
+RenderProjects();
---
+++ src/js/projects.js
@@ -0,0 +1,47 @@
+import london from "../images/projects/london.jpg";
+import birmingham from "../images/projects/birmingham.jpg";
+import brighton from "../images/projects/brighton.jpg";
+import manchester from "../images/projects/manchester.jpg";
+import newcastle from "../images/projects/newcastle.jpg";
+
+const projects = [
+  {
+    title: "London",
+    image: london,
+    description: "A project based in the city of London",
+  },
+  {
+    title: "Birmingham",
+    image: birmingham,
+    description: "A project based in the city of Birmingham",
+  },
+  {
+    title: "Brighton",
+    image: brighton,
+    description: "A project based in the city of Brighton",
+  },
+  {
+    title: "Manchester",
+    image: manchester,
+    description: "A project based in the city of Manchester",
+  },
+  {
+    title: "Newcastle",
+    image: newcastle,
+    description: "A project based in the city of Newcastle",
+  },
+];
+
+export function RenderProjects() {
+  const grid = document.querySelector(".projects-grid");
+
+  projects.forEach((project) => {
+    grid.innerHTML += `
+    <article class="project-card">
+      <img src="${project.image}" alt="${project.title}">
+      <h3>${project.title}</h3>
+      <p>${project.description}</p>
+    </article>
+  `;
+  });
+}
