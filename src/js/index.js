3b3703322cf6ba5e27eab61bb07dccabae777a6e
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-07-22 12:15:41 +0100

    Create contact css file & link to project

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../css/index.css";
 import "../css/about.css";
 import "../css/projects.css";
+import "../css/contact.css";
 import { RenderProjects } from "./projects.js";
 import { initForm } from "@formspree/ajax";
 
