b5dab585b46dc525a87e7c7c571063ee10ccff17
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-07-22 12:14:33 +0100

    Link contact form to formspree

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,15 @@ import "../css/index.css";
 import "../css/about.css";
 import "../css/projects.css";
 import { RenderProjects } from "./projects.js";
+import { initForm } from "@formspree/ajax";
+
+const contactForm = document.querySelector("#contact-form");
+
+if (contactForm) {
+  initForm({
+    formElement: contactForm,
+    formId: "mzdnabvj",
+  });
+}
 
 RenderProjects();
