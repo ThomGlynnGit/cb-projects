618591739d5ed5d6864296df07e546d98747113f
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-07-22 12:43:13 +0100

    Fix bug where formspree did not work with webpack

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,5 @@ import "../css/about.css";
 import "../css/projects.css";
 import "../css/contact.css";
 import { RenderProjects } from "./projects.js";
-import { initForm } from "@formspree/ajax";
-
-const contactForm = document.querySelector("#contact-form");
-
-if (contactForm) {
-  initForm({
-    formElement: contactForm,
-    formId: "mzdnabvj",
-  });
-}
 
 RenderProjects();
